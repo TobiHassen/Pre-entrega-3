78279262d9c887dd732ded8b0fd14e825a224f92
tobias hassen <tobiashassencoder@gmail.com>
2023-04-25 14:37:05 -0300

    cree promesas para agregar producto al carrito y logre que sea funcional

--- carrito.js
+++ carrito.js
@@ -12,56 +12,63 @@ const botonComprar = document.querySelector("#carrito-acciones-comprar");
 
 
 function cargarProductosCarrito() {
-    if (productosEnCarrito && productosEnCarrito.length > 0) {
+    return new Promise((resolve, reject) => {
+        if (productosEnCarrito && productosEnCarrito.length > 0) {
 
-        contenedorCarritoVacio.classList.add("disabled");
-        contenedorCarritoProductos.classList.remove("disabled");
-        contenedorCarritoAcciones.classList.remove("disabled");
-        contenedorCarritoComprado.classList.add("disabled");
+            contenedorCarritoVacio.classList.add("disabled");
+            contenedorCarritoProductos.classList.remove("disabled");
+            contenedorCarritoAcciones.classList.remove("disabled");
+            contenedorCarritoComprado.classList.add("disabled");
 
-        contenedorCarritoProductos.innerHTML = "";
+            contenedorCarritoProductos.innerHTML = "";
 
-        productosEnCarrito.forEach(producto => {
+            productosEnCarrito.forEach(producto => {
 
-            const div = document.createElement("div");
-            div.classList.add("carrito-producto");
-            div.innerHTML = `
-                <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
-                <div class="carrito-producto-titulo">
-                    <small>Título</small>
-                    <h3>${producto.titulo}</h3>
-                </div>
-                <div class="carrito-producto-cantidad">
-                    <small>Cantidad</small>
-                    <p>${producto.cantidad}</p>
-                </div>
-                <div class="carrito-producto-precio">
-                    <small>Precio</small>
-                    <p>$${producto.precio}</p>
-                </div>
-                <div class="carrito-producto-subtotal">
-                    <small>Subtotal</small>
-                    <p>$${producto.precio * producto.cantidad}</p>
-                </div>
-                <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
-            `;
+                const div = document.createElement("div");
+                div.classList.add("carrito-producto");
+                div.innerHTML = `
+            <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="carrito-producto-titulo">
+                <small>Título</small>
+                <h3>${producto.titulo}</h3>
+            </div>
+            <div class="carrito-producto-cantidad">
+                <small>Cantidad</small>
+                <p>${producto.cantidad}</p>
+            </div>
+            <div class="carrito-producto-precio">
+                <small>Precio</small>
+                <p>$${producto.precio}</p>
+            </div>
+            <div class="carrito-producto-subtotal">
+                <small>Subtotal</small>
+                <p>$${producto.precio * producto.cantidad}</p>
+            </div>
+            <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
+        `;
 
-            contenedorCarritoProductos.append(div);
-        })
+                contenedorCarritoProductos.append(div);
+            })
 
-        actualizarBotonesEliminar();
-        actualizarTotal();
+            actualizarBotonesEliminar();
+            actualizarTotal();
 
-    } else {
-        contenedorCarritoVacio.classList.remove("disabled");
-        contenedorCarritoProductos.classList.add("disabled");
-        contenedorCarritoAcciones.classList.add("disabled");
-        contenedorCarritoComprado.classList.add("disabled");
-    }
+            resolve();
 
+        } else {
+            contenedorCarritoVacio.classList.remove("disabled");
+            contenedorCarritoProductos.classList.add("disabled");
+            contenedorCarritoAcciones.classList.add("disabled");
+            contenedorCarritoComprado.classList.add("disabled");
+
+            reject(new Error("No hay productos en el carrito"));
+        }
+    });
 }
 
-cargarProductosCarrito();
+cargarProductosCarrito().catch(error => {
+    console.error(error.message);
+});
 
 function actualizarBotonesEliminar() {
     botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
